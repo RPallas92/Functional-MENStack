b9db23d70af7b70a381322798d8a3f862faee658
rpallas <rpallas@hiberus.com>
2016-02-22 12:56:24 +0100

    Now, the GetPagedAnimals use case is pure

--- src/domain/interactor/GetPagedAnimals.js
+++ src/domain/interactor/GetPagedAnimals.js
@@ -1,9 +1,8 @@
 var AnimalRepository = require('../../data/AnimalRepository.js')
-var PAGE_SIZE = 2;
-var DEFAULT_PAGE_NUMBER = 1
-
 
 var execute = function(pageNumber){
+	var PAGE_SIZE = 2;
+	var DEFAULT_PAGE_NUMBER = 1
 	var cleanPageNumber = pageNumber >= 1 ? pageNumber : DEFAULT_PAGE_NUMBER
 	return AnimalRepository.getPagedAnimalsByPage(cleanPageNumber, PAGE_SIZE)
 }
