8cade767df13f0d7815a23727f1dcd56bb446723
Saurabh SHarma <saurshaz@gmail.com>
2016-05-06 15:05:58 +0530

    Changes to fix fresh setup
    - add mongodb dependency
    - correct path as per README to run migration script

--- db_scripts/importData.sh
+++ db_scripts/importData.sh
@@ -1,3 +1,3 @@
-mongoimport --host=127.0.0.1 --db farm --collection animals --file animals.json --jsonArray
-mongoimport --host=127.0.0.1 --db farm --collection owners --file owners.json --jsonArray
-mongoimport --host=127.0.0.1 --db farm --collection vaccines --file vaccines.json --jsonArray
+mongoimport --host=127.0.0.1 --db farm --collection animals --file db_scripts/animals.json --jsonArray
+mongoimport --host=127.0.0.1 --db farm --collection owners --file db_scripts/owners.json --jsonArray
+mongoimport --host=127.0.0.1 --db farm --collection vaccines --file db_scripts/vaccines.json --jsonArray
